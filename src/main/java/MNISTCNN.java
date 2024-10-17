fe52ea24acc01e7b35d7fa077755bcd1658d6cfb
rhit-mostelcj <mostelcj@rose-hulman.edu>
2024-10-17 18:18:07 -0400

    reading all files

--- src/main/java/MNISTCNN.java
+++ src/main/java/MNISTCNN.java
@@ -1,2 +1,96 @@
+import javax.swing.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class MNISTCNN {
+
+    private static final int TESTINGSIZE = 10;
+
+    public static void main(String[] args) {
+        String trainImagesPath = "train-images-idx3-ubyte";
+        String trainLabelsPath = "train-labels-idx1-ubyte";
+        try {
+            readImages(trainImagesPath);
+            readLabels(trainLabelsPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readImages(String filePath) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+
+        // Read header
+        int magicNumber = readInt(fis);
+        int numberOfImages = readInt(fis);
+        int numberOfRows = readInt(fis);
+        int numberOfColumns = readInt(fis);
+
+        numberOfImages = TESTINGSIZE;
+
+        System.out.println("Magic Number: " + magicNumber);
+        System.out.println("Number of Images: " + numberOfImages);
+        System.out.println("Image Size: " + numberOfRows + "x" + numberOfColumns);
+
+        // Read images
+        for (int i = 0; i < numberOfImages; i++) {
+            int[][] image = new int[numberOfRows][numberOfColumns];
+            for (int row = 0; row < numberOfRows; row++) {
+                for (int col = 0; col < numberOfColumns; col++) {
+                    image[row][col] = fis.read() & 0xFF; // Convert to unsigned byte
+                }
+            }
+            // Process the image here (e.g., display or save it)
+            System.out.println("Read image " + (i + 1));
+            displayImage(image, numberOfColumns, numberOfRows);
+        }
+
+        fis.close();
+    }
+
+    public static void readLabels(String filePath) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+
+        // Read header
+        int magicNumber = readInt(fis);
+        int numberOfItems = readInt(fis);
+
+        numberOfItems = TESTINGSIZE;
+
+        System.out.println("Magic Number: " + magicNumber);
+        System.out.println("Number of Labels: " + numberOfItems);
+
+        // Read labels
+        for (int i = 0; i < numberOfItems; i++) {
+            int label = fis.read() & 0xFF; // Convert to unsigned byte
+            System.out.println("Label " + (i + 1) + ": " + label);
+        }
+
+        fis.close();
+    }
+
+    private static int readInt(FileInputStream fis) throws IOException {
+        return (fis.read() << 24) | (fis.read() << 16) | (fis.read() << 8) | fis.read();
+    }
+
+    public static void displayImage(int[][] image, int width, int height) {
+        JFrame window = new JFrame("foo");
+        window.setSize((width+2)*16, (height+2)*16 + 20);
+        PixelGrid pGrid = drawImage(image, width, height);
+        window.add(pGrid);
+        window.setVisible(true);
+        window.repaint();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static PixelGrid drawImage(int[][] image, int width, int height) {
+        PixelGrid grid = new PixelGrid(width, height);
+        for(int i = 0; i<28; i++) {
+            for(int j = 0; j<28; j++) {
+                grid.setPixel(image[i][j],j,i);
+            }
+        }
+        return grid;
+    }
 }
---
+++ src/main/java/PixelGrid.java
@@ -0,0 +1,35 @@
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class PixelGrid extends JPanel {
+
+    public BufferedImage grid;
+    int PIXEL_SIZE = 16;
+
+    public PixelGrid(int width, int height) {
+        grid = new BufferedImage(width*PIXEL_SIZE, height*PIXEL_SIZE, BufferedImage.TYPE_BYTE_GRAY);
+    }
+
+    public void setPixel(int color, int x, int y) {
+        color = 255-color;
+        x = convertGridIndexToActual(x);
+        y = convertGridIndexToActual(y);
+        for(int i = 0; i<PIXEL_SIZE; i++) {
+            for(int j = 0; j<PIXEL_SIZE; j++) {
+                grid.setRGB((x + i)%grid.getWidth(), (y+ j) % grid.getHeight() , (int)(color << 16 | color << 8 | color));
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(grid, 10, 10, this);
+    }
+
+    public  int convertGridIndexToActual(int x) {
+        return x*PIXEL_SIZE;
+    }
+}
